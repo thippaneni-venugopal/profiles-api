f6389103150bd7ade6bc7e07d25cc99f0069b9ee
Venugpal-Thippaneni <vthippaneni@verisk.com>
2024-12-05 18:16:23 +0530

    Added Hello api View Set

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+
 urlpatterns = [
-  path('hello/', views.HelloApiView.as_view()),
+  path('hello-view/', views.HelloApiView.as_view()),
+  path('', include(router.urls))
 ]
