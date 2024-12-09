663419d396f4f10a430e64bd3dca31a7c3a50938
Venugpal-Thippaneni <vthippaneni@verisk.com>
2024-12-09 17:20:49 +0530

    added search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from profiles_api import serializers, models
 
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
+from rest_framework import filters
 
 
 class HelloApiView(APIView):
@@ -117,5 +118,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
   authentication_classes = (TokenAuthentication,)
   permission_classes = (permissions.UpdateOwnProfile,)
+  filter_backends = (filters.SearchFilter, )
+  search_fields = ('name', 'email',)
+
 
 
